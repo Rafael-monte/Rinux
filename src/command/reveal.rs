3d7691c0f54085bf3e92219102aad7e0cc5a25f6
Rafael-monte <rafaelmonte46@gmail.com>
2024-04-08 23:46:37 -0300

    Changes in structure. Added locus.

--- src/command/reveal.rs
+++ src/command/reveal.rs
@@ -1,19 +1,41 @@
 pub mod internal {
     use std::ffi::OsStr;
-    use std::path::Path;
+    use std::fs::DirEntry;
+    use std::io::ErrorKind;
+    use std::path::{Display, Path, PathBuf};
     use crate::command::traits::EssentialCommand;
 
     pub struct Reveal {
         protected_mode: bool,
         args: Vec<String>
     }
-
     impl Reveal {
         pub fn spawn() -> Self {
             return Self {
                 protected_mode: false,
                 args: Vec::new()
             }
+        }
+        fn adjust_locus(&self, locus: &&str) -> Result<PathBuf, ErrorKind> {
+            let complete_path = std::fs::canonicalize(*locus);
+            if complete_path.is_err() {
+                eprintln!("Couldn't construct the provided locus: \"{}\"", *locus);
+                return Err(ErrorKind::InvalidData);
+            }
+            return Ok(complete_path.unwrap());
+        }
+
+        fn reveal_entry(&self, entry: &DirEntry) -> () {
+            const FORMATTING_ERROR: &str = "(Couldn't format value)";
+            let entry_p = entry.path();
+            let is_directory = entry_p.is_dir();
+            let mut prefix = "📦";
+            let mut extension = OsStr::new("Package");
+            if !is_directory {
+                prefix = "📄";
+                extension = entry_p.extension().unwrap_or(OsStr::new("Unknown"));
+            }
+            println!("{} {} - {}", prefix, entry.path().display(), extension.to_str().unwrap_or(FORMATTING_ERROR));
         }
     }
 
@@ -39,31 +61,29 @@ pub mod internal {
         }
 
         fn run(&mut self, args: &[&str]) -> () {
-            const FORMATTING_ERROR: &str = "(Couldn't format value)";
-            let mut current_path = Path::new(".");
-            let mut path_name = "current path";
+            const CURRENT_PATH: &str=".";
+            let mut current_path = Path::new(CURRENT_PATH);
+            let mut canonical_path = std::fs::canonicalize(current_path).unwrap();
             if let Some(locus) = args.first() {
-                path_name = *locus;
-                todo!("Locus not yet implemented");
-            }
-            if let Ok(directory) = std::fs::read_dir(current_path) {
-                println!("🔎📦 Looking into {}", path_name);
-                for opt_entry in directory {
-                    if let Ok(entry) = opt_entry {
-                        let entry_p = entry.path();
-                        let is_directory = entry_p.is_dir();
-                        let mut prefix = "📦";
-                        let mut extension = OsStr::new("Directory");
-                        if !is_directory {
-                            prefix = "📄";
-                            extension = entry_p.extension().unwrap_or(OsStr::new("Unknown"));
-                        }
-                        println!("{} {} - {}", prefix, entry.path().display(), extension.to_str().unwrap_or(FORMATTING_ERROR));
-                    }
+                let complete_path = self.adjust_locus(locus);
+                if complete_path.is_err() {
+                    return;
                 }
+                canonical_path = complete_path.unwrap();
+            }
+            let _path_name_binding = canonical_path.clone();
+            let path_name: Display<'_> = _path_name_binding.display();
+            let directory = std::fs::read_dir(canonical_path);
+            if directory.is_err() {
+                eprintln!("Couldn't open directory \"{}\"", current_path.display());
                 return;
             }
-            eprintln!("Couldn't open directory \"{}\"", current_path.display());
+            println!("🔎 Looking into {}", path_name);
+            for opt_entry in directory.unwrap() {
+                if let Ok(entry) = opt_entry {
+                    self.reveal_entry(&entry);
+                }
+            }
         }
 
         fn get_full_name(&self) -> String {
